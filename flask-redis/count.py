db7601eee3928fc8d9898e6a39babef2f64b6a8e
kaaamh96 <ahussainkamrul@hotmail.com>
2025-10-06 20:00:17 +0100

    newcommand and conf file for loadbalancing nginx

--- flask-redis/count.py
+++ flask-redis/count.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
 import redis
 
 app = Flask(__name__)
-r = redis.Redis(host='redis', port=6379)
+redis_host = os.getenv('REDIS_HOST', 'redis')
+redis_port = int(os.getenv('REDIS_PORT', 6379))
+r = redis.Redis(host=redis_host, port=redis_port)
 
 @app.route ('/') 
 def welcome():
